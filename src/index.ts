fbecab2f84424d8b7a740ba6cc6cc2ab542cd8ff
johngalang <imman.galang@gmail.com>
2023-04-23 10:49:19 +0800

    LOM-5 setup prisma

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,40 @@
+import { PrismaClient } from '@prisma/client';
 import express, { Application, Request, Response } from 'express';
-import pg, { QueryResult } from 'pg';
 
 class Server {
   private app: Application;
-  private db: pg.Pool;
+  private prisma: PrismaClient;
 
   constructor() {
     this.app = express();
     this.config();
     this.routes();
-    this.db = new pg.Pool({
-      user: 'postgres',
-      host: 'localhost',
-      database: 'lawomdb',
-      password: 'mypassword',
-      port: 5432,
-    });
+    this.prisma = new PrismaClient();
   }
 
   private async testDbConnection(): Promise<void> {
-    const queryResult: QueryResult = await this.db.query('SELECT NOW()');
-    console.log(`Connected to database at ${queryResult.rows[0].now}`);
+    try {
+      await this.prisma.$connect();
+      console.log('Database connection successful!');
+    } catch (e) {
+      console.error('Database connection failed!', e);
+    } finally {
+      await this.prisma.$disconnect();
+    }
   }
 
   private config() {
-    // Add any necessary configuration here
     this.app.use(express.json());
   }
 
   private routes() {
-    // Add your routes here
     this.app.get('/', async (req: Request, res: Response) => {
-      const queryResult: QueryResult = await this.db.query('SELECT * FROM mytable');
-      res.json(queryResult.rows);
+      const users = await this.prisma.user.findMany();
+      res.json('hello world');
     });
   }
 
   start() {
-    // Start the server
     const port = process.env.PORT || 3000;
     this.app.listen(port, () => {
       console.log(`Server listening on port ${port}`);

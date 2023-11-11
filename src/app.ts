ce21e8061cd72db4e42247674753c12ea0c0a443
johngalang <imman.galang@gmail.com>
2023-11-11 15:03:52 -0500

    add validation middleware for req.body

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { getUsersCollection } from './db';
 import { hash } from 'bcrypt';
 import { z } from 'zod';
@@ -7,7 +7,28 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', async (req, res) => {
+
+const User = z.object({
+  name: z.string(),
+  email: z.string().email('Invalid email'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+})
+
+const isUserAlreadyRegistered = async (email: string) => {
+  const user = await getUsersCollection('users').findOne({ email })
+  return user
+}
+
+const validateSchema = (schema: z.ZodSchema<any>) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    schema.parse(req.body)
+    next()
+  } catch (error) {
+    res.status(400).send({ message: error.message })
+  }
+}
+
+app.post('/auth/register', validateSchema(User), async (req: Request, res: Response) => {
   try {
     const { name, email, password } = User.parse(req.body);
     const existingUser = await isUserAlreadyRegistered(email)
@@ -24,16 +45,5 @@ app.post('/auth/register', async (req, res) => {
     res.status(500).send({ message: 'Error creating user' })
   }
 })
-
-const User = z.object({
-  name: z.string(),
-  email: z.string().email('Invalid email'),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
-})
-
-const isUserAlreadyRegistered = async (email: string) => {
-  const user = await getUsersCollection('users').findOne({ email })
-  return user
-}
 
 export default app;
